aa3092bd82ad7d371c04cae52cb79fa628c98200
Kaizen256 <rowekaizen@gmail.com>
2024-05-05 22:05:19 -0700

    Bad_torch_vision_model

---
+++ Terrible_first_torch_vision/First_torchvision_Terrible.py
@@ -0,0 +1,259 @@
+import torch
+from torch import nn
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from helper_functions import accuracy_fn
+
+train_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=None
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False, # get test data
+    download=True,
+    transform=ToTensor()
+)
+
+from timeit import default_timer as timer 
+def print_train_time(start: float, end: float, device: torch.device = None):
+    """Prints difference between start and end time.
+
+    Args:
+        start (float): Start time of computation (preferred in timeit format). 
+        end (float): End time of computation.
+        device ([type], optional): Device that compute is running on. Defaults to None.
+
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
+
+class_names = train_data.classes
+"""fig, axes = plt.subplots(4, 4, figsize=(10, 10))
+for i, ax in enumerate(axes.flat):  # Flatten the 4x4 array of axes
+    image, label = train_data[i]  # Assuming train_data contains your images and labels
+    ax.imshow(image.squeeze(), cmap="gray")  # Display the image
+    ax.set_title(class_names[label])  # Set title as per the label
+
+plt.tight_layout()  # Adjust layout to prevent overlapping
+plt.show()
+"""
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(train_data,
+                              batch_size=32,
+                              shuffle=True)
+
+test_dataloader = DataLoader(test_data,
+                              batch_size=32,
+                              shuffle=False)
+
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
+
+"""torch.manual_seed(42)
+random_idx = torch.randint(0, len(train_features_batch), size=[1]).item()
+img, label = train_features_batch[random_idx], train_labels_batch[random_idx]
+plt.imshow(img.squeeze(), cmap="gray")
+plt.title(class_names[label])
+plt.axis("Off")
+plt.show()
+print(f"Image size: {img.shape}")
+print(f"Label: {label}, label size: {label.shape}")"""
+
+flatten_model = nn.Flatten()
+x = train_features_batch[0] #Shape = [1, 28, 28]
+output = flatten_model(x) #Shape = [1, 784]
+
+"""
+class FashionMNISTModel(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=input_shape, out_features=hidden_units),
+            nn.Linear(in_features=hidden_units, out_features=output_shape)
+        )
+    def forward(self, x):
+        return self.layer_stack(x)
+    
+torch.manual_seed(42)
+
+model = FashionMNISTModel(input_shape=784,
+                          hidden_units=10,
+                          output_shape=len(class_names))
+model.to("cpu")
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+
+torch.manual_seed(42)
+train_time_start_on_cpu = timer()
+epochs = 3
+for epoch in range(epochs):
+    print(f"Epoch: {epoch}\n-------")
+    train_loss = 0
+    for batch, (X, y) in enumerate(train_dataloader):
+        model.train() 
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch % 400 == 0:
+            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
+
+    train_loss /= len(train_dataloader)
+    
+    test_loss, test_acc = 0, 0 
+    model.eval()
+    with torch.inference_mode():
+        for X, y in test_dataloader:
+            test_pred = model(X)
+           
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+
+        test_loss /= len(test_dataloader)
+        test_acc /= len(test_dataloader)
+
+    print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
+    
+train_time_end_on_cpu = timer()
+total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
+                                           end=train_time_end_on_cpu,
+                                           device=str(next(model.parameters()).device))
+
+"""
+class FashionMNISTModelV1(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=input_shape, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_shape),
+            nn.ReLU()
+        )
+    
+    def forward(self, x: torch.Tensor):
+        return self.layer_stack(x)
+
+torch.manual_seed(42)
+model_1 = FashionMNISTModelV1(input_shape=784,
+                              hidden_units=10,
+                              output_shape=len(class_names)).to("cuda")
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_1.parameters(), 
+                            lr=0.1)
+
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device = "cuda"):
+    train_loss, train_acc = 0, 0
+    model.to(device)
+    for batch, (X, y) in enumerate(data_loader):
+        X, y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc += accuracy_fn(y_true=y,
+                                 y_pred=y_pred.argmax(dim=1)) # Go from logits -> pred labels
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+def test_step(data_loader: torch.utils.data.DataLoader,
+              model: torch.nn.Module,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device: torch.device = "cuda"):
+    test_loss, test_acc = 0, 0
+    model.to(device)
+    model.eval() # put model in eval mode
+    with torch.inference_mode(): 
+        for X, y in data_loader:
+            X, y = X.to(device), y.to(device)
+
+            test_pred = model(X)
+
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y_true=y,
+                y_pred=test_pred.argmax(dim=1)
+            )
+
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
+epochs = 3
+train_time_start_on_gpu = timer()
+for epoch in range(epochs):
+    print(f"Epoch: {epoch}\n---------")
+    train_step(data_loader=train_dataloader, 
+        model=model_1, 
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn
+    )
+    test_step(data_loader=test_dataloader,
+        model=model_1,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn
+    )
+
+train_time_end_on_gpu = timer()
+total_train_time_model_1 = print_train_time(start=train_time_start_on_gpu,
+                                            end=train_time_end_on_gpu,
+                                            device="cuda")
+
+torch.manual_seed(42)
+def eval_model(model: torch.nn.Module, 
+               data_loader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               accuracy_fn, 
+               device: torch.device = "cuda"):
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Send data to the target device
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+        
+        # Scale loss and acc
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+    return {"model_name": model.__class__.__name__, # only works when model was created with a class
+            "model_loss": loss.item(),
+            "model_acc": acc}
+
+# Calculate model 1 results with device-agnostic code 
+model_1_results = eval_model(model=model_1, data_loader=test_dataloader,
+    loss_fn=loss_fn, accuracy_fn=accuracy_fn,
+    device="cuda"
+)
+print(model_1_results)
