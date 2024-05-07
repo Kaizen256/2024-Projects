ed37cf35494595b686cbb82ab68fc6a4a81928e0
Kaizen256 <rowekaizen@gmail.com>
2024-05-06 21:21:02 -0700

    Convolutional Neural Network

---
+++ 1st_Convolutional_Neural_Network/CNN.py
@@ -0,0 +1,190 @@
+import torch
+from torch import nn
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+train_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=None
+)
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False, # get test data
+    download=True,
+    transform=ToTensor()
+)
+
+class_names = train_data.classes
+train_dataloader = DataLoader(train_data,
+                              batch_size=32,
+                              shuffle=True)
+
+test_dataloader = DataLoader(test_data,
+                              batch_size=32,
+                              shuffle=False)
+
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
+
+flatten_model = nn.Flatten()
+x = train_features_batch[0] #Shape = [1, 28, 28]
+output = flatten_model(x) #Shape = [1, 784]
+
+torch.manual_seed(42)
+
+class Convolutional(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.classifier(x)
+        return x
+    
+model = Convolutional(input_shape=1,
+                        hidden_units=10,
+                        output_shape=len(class_names)).to("cuda")
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(), 
+                             lr=0.05)
+
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device = "cuda"):
+    train_loss, train_acc = 0, 0
+    model.to(device)
+    for batch, (X, y) in enumerate(data_loader):
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc += accuracy_fn(y_true=y,
+                                 y_pred=y_pred.argmax(dim=1))
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+def test_step(data_loader: torch.utils.data.DataLoader,
+              model: torch.nn.Module,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device: torch.device = "cuda"):
+    test_loss, test_acc = 0, 0
+    model.to(device)
+    model.eval()
+    with torch.inference_mode(): 
+        for X, y in data_loader:
+            X, y = X.to(device), y.to(device)
+            test_pred = model(X)
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y_true=y,
+                y_pred=test_pred.argmax(dim=1)
+            )
+
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
+
+from timeit import default_timer as timer
+def print_train_time(start: float, end: float, device: torch.device = None):
+    """Prints difference between start and end time.
+
+    Args:
+        start (float): Start time of computation (preferred in timeit format). 
+        end (float): End time of computation.
+        device ([type], optional): Device that compute is running on. Defaults to None.
+
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
+train_time_start_model_2 = timer()
+
+epochs = 75
+for epoch in range(epochs):
+    print(f"Epoch: {epoch}\n---------")
+    train_step(data_loader=train_dataloader, 
+        model=model, 
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn,
+        device="cuda"
+    )
+    test_step(data_loader=test_dataloader,
+        model=model,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn,
+        device="cuda"
+    )
+
+train_time_end_model_2 = timer()
+total_train_time_model_2 = print_train_time(start=train_time_start_model_2,
+                                           end=train_time_end_model_2,
+                                           device="cuda")
+
+from pathlib import Path
+
+# 1. Create models directory 
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+# 2. Create model save path 
+MODEL_NAME = "CNN01.path"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# 3. Save the model state dict 
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model.state_dict(), # only saving the state_dict() only saves the models learned parameters
+           f=MODEL_SAVE_PATH)
+
