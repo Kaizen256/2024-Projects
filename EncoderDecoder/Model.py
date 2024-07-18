774170107b981cad0e4f230a07adf4d5ad7a3f0d
Kaizen256 <rowekaizen@gmail.com>
2024-07-18 14:08:45 -0700

    commit to laptop

--- EncoderDecoder/Model.py
+++ EncoderDecoder/Model.py
@@ -15,5 +15,58 @@ target_train = full_train[:, 2:]
 train_data = TensorDataset(full_train, target_train)
 generator = torch.Generator()
 train_loader = DataLoader(train_data, batch_size=16, shuffle=True, generator=generator)
-_points
-test
+
+test_points, test_directions = generate_sequences(seed=19)
+full_test = torch.as_tensor(test_points).float()
+source_test = full_test[:, :2]
+target_test = full_test[:, 2:]
+test_data = TensorDataset(source_test, target_test)
+test_loader = DataLoader(test_data, batch_size=16)
+
+class EncoderDecoderSelfAttn(nn.Module):
+    def __init__(self, encoder, decoder, input_len, target_len):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.input_len = input_len
+        self.target_len = target_len
+        self.trg_masks = self.subsequent_mask(self.target_len)
+      
+    @staticmethod
+    def subsequent_mask(size):
+        attn_shape = (1, size, size)
+        subsequent_mask = (1 - torch.triu(torch.ones(attn_shape), diagnol=1))
+        return subsequent_mask
+    
+    def encode(self, source_seq, source_mask):
+        encoder_states = self.encoder(source_seq, source_mask)
+        self.decoder.init_keys(encoder_states)
+
+    def decode(self, shifted_target_seq, source_mask=None, target_mask=None):
+        outputs = self.decoder(shifted_target_seq,
+                               source_mask=source_mask,
+                               target_mask=target_mask)
+        return outputs
+    
+    def predict(self, source_seq, source_mask):
+        inputs = source_seq[:, -1:]
+        for i in range(self.target_len):
+            out = self.decode(inputs, source_mask, self.trg_masks[:, :i+1, :i+1])
+            out = torch.cat([inputs, out[:, -1:, :]], dim=-2)
+            inputs = out.detach()
+        outputs = inputs[:, 1:, :]
+        return outputs
+    
+    def forward(self, X, source_mask=None):
+        self.trg_masks = self.trg_masks.type_as(X).bool()
+        source_seq = X[:, :self.input_len, :]
+        self.encode(source_seq, source_mask)
+        if self.training:
+          shifted_target_seq = X[:, self.input_len-1:-1, :]
+          outputs = self.decode(shifted_target_seq, source_mask, self.trg_masks)
+        else:
+            outputs = self.predict(source_seq, source_mask)
+        return outputs
+    
+class PotionalEncoding(nn.Module):
+    def __init__(self, max_len, d_model)
