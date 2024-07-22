cc58f1353b30aaa965237e2bf11bf416513718ad
Kaizen256 <rowekaizen@gmail.com>
2024-07-21 23:16:21 -0700

    Encoder Decoder

--- EncoderDecoder/Model.py
+++ EncoderDecoder/Model.py
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader, Dataset, random_split, \
 from squaresequences import generate_sequences
 from stepbystep import StepByStep
 
-points, directions = generate_sequences(n=512)
+points, directions = generate_sequences(n=256)
 full_train = torch.as_tensor(points).float()
 target_train = full_train[:, 2:]
 train_data = TensorDataset(full_train, target_train)
@@ -35,7 +35,7 @@ class EncoderDecoderSelfAttn(nn.Module):
     @staticmethod
     def subsequent_mask(size):
         attn_shape = (1, size, size)
-        subsequent_mask = (1 - torch.triu(torch.ones(attn_shape), diagnol=1))
+        subsequent_mask = (1 - torch.triu(torch.ones(attn_shape), diagonal=1)).bool()
         return subsequent_mask
     
     def encode(self, source_seq, source_mask):
@@ -68,5 +68,170 @@ class EncoderDecoderSelfAttn(nn.Module):
             outputs = self.predict(source_seq, source_mask)
         return outputs
     
-class PotionalEncoding(nn.Module):
-    def __init__(self, max_len, d_model)
+class PositionalEncoding(nn.Module):
+    def __init__(self, max_len, d_model):
+        super().__init__()
+        self.d_model = d_model
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        angular_speed = torch.exp(torch.arange(0, d_model, 2).float() * (-np.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * angular_speed) # even dimensions
+        pe[:, 1::2] = torch.cos(position * angular_speed) # odd dimensions
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        # x is N, L, D
+        # pe is 1, maxlen, D
+        scaled_x = x * np.sqrt(self.d_model)
+        encoded = scaled_x + self.pe[:, :x.size(1), :]
+        return encoded
+
+class EncoderSelfAttn(nn.Module):
+    def __init__(self, n_heads, d_model, ff_units, n_features=None):
+        super().__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.ff_units = ff_units
+        self.n_features = n_features
+        self.self_attn_heads = MultiHeadAttention(n_heads, d_model, input_dim=n_features)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, ff_units),
+            nn.ReLU(),
+            nn.Linear(ff_units, d_model),
+        )
+         
+    def forward(self, query, mask=None):
+        self.self_attn_heads.init_keys(query)
+        att = self.self_attn_heads(query, mask)
+        out = self.ffn(att)
+        return out
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, d_model, input_dim=None, proj_values=True):
+        super().__init__()
+        self.linear_out = nn.Linear(n_heads * d_model, d_model)
+        self.attn_heads = nn.ModuleList([Attention(d_model, 
+                                                   input_dim=input_dim, 
+                                                   proj_values=proj_values) 
+                                         for _ in range(n_heads)])
+        
+    def init_keys(self, key):
+        for attn in self.attn_heads:
+            attn.init_keys(key)
+        
+    @property
+    def alphas(self):
+        # Shape: n_heads, N, 1, L (source)
+        return torch.stack([attn.alphas for attn in self.attn_heads], dim=0)
+        
+    def output_function(self, contexts):
+        # N, 1, n_heads * D
+        concatenated = torch.cat(contexts, axis=-1)
+        out = self.linear_out(concatenated) # N, 1, D
+        return out
+        
+    def forward(self, query, mask=None):
+        contexts = [attn(query, mask=mask) for attn in self.attn_heads]
+        out = self.output_function(contexts)
+        return out
+    
+class Attention(nn.Module):
+    def __init__(self, hidden_dim, input_dim=None, proj_values=False):
+        super().__init__()
+        self.d_k = hidden_dim
+        self.input_dim = hidden_dim if input_dim is None else input_dim
+        self.proj_values = proj_values
+        self.linear_query = nn.Linear(self.input_dim, hidden_dim)
+        self.linear_key = nn.Linear(self.input_dim, hidden_dim)
+        self.linear_value = nn.Linear(self.input_dim, hidden_dim)
+        self.alphas = None
+                
+    def init_keys(self, keys):
+        self.keys = keys
+        self.proj_keys = self.linear_key(self.keys)
+        self.values = self.linear_value(self.keys) \
+                      if self.proj_values else self.keys
+                
+    def score_function(self, query):
+        proj_query = self.linear_query(query)
+        # scaled dot product
+        # N, 1, H x N, H, L -> N, 1, L
+        dot_products = torch.bmm(proj_query, self.proj_keys.permute(0, 2, 1))
+        scores =  dot_products / np.sqrt(self.d_k)
+        return scores
+            
+    def forward(self, query, mask=None):
+        # Query is batch-first N, 1, H
+        scores = self.score_function(query) # N, 1, L
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+        alphas = F.softmax(scores, dim=-1) # N, 1, L
+        self.alphas = alphas.detach()
+        
+        # N, 1, L x N, L, H -> N, 1, H
+        context = torch.bmm(alphas, self.values)
+        return context
+
+class DecoderSelfAttn(nn.Module):
+    def __init__(self, n_heads, d_model, ff_units, n_features=None):
+        super().__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.ff_units = ff_units
+        self.n_features = d_model if n_features is None else n_features
+        self.self_attn_heads = MultiHeadAttention(n_heads, d_model, input_dim=self.n_features)
+        self.cross_attn_heads = MultiHeadAttention(n_heads, d_model)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, ff_units),
+            nn.ReLU(),
+            nn.Linear(ff_units, self.n_features),
+        )
+        
+    def init_keys(self, states):
+        self.cross_attn_heads.init_keys(states)
+         
+    def forward(self, query, source_mask=None, target_mask=None):
+        self.self_attn_heads.init_keys(query)
+        att1 = self.self_attn_heads(query, target_mask)
+        att2 = self.cross_attn_heads(att1, source_mask)
+        out = self.ffn(att2)
+        return out
+    
+class EncoderPe(nn.Module):
+    def __init__(self, n_heads, d_model, ff_units, n_features=None, max_len=100):
+        super().__init__()
+        pe_dim = d_model if n_features is None else n_features
+        self.pe = PositionalEncoding(max_len, pe_dim)
+        self.layer = EncoderSelfAttn(n_heads, d_model, ff_units, n_features)
+    
+    def forward(self, query, mask=None):
+        query_pe = self.pe(query)
+        out = self.layer(query_pe, mask)
+        return out
+    
+class DecoderPe(nn.Module):
+    def __init__(self, n_heads, d_model, ff_units, n_features=None, max_len=100):
+        super().__init__()
+        pe_dim = d_model if n_features is None else n_features
+        self.pe = PositionalEncoding(max_len, pe_dim)
+        self.layer = DecoderSelfAttn(n_heads, d_model, ff_units, n_features)
+        
+    def init_keys(self, states):
+        self.layer.init_keys(states)
+    
+    def forward(self, query, source_mask=None, target_mask=None):
+        query_pe = self.pe(query)
+        out = self.layer(query_pe, source_mask, target_mask)
+        return out
+  
+torch.manual_seed(43)
+encpe = EncoderPe(n_heads=3, d_model=2, ff_units=10, n_features=2)
+decpe = DecoderPe(n_heads=3, d_model=2, ff_units=10, n_features=2)
+
+model = EncoderDecoderSelfAttn(encpe, decpe, input_len=2, target_len=2)
+loss = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+sbs_seq_selfattnpe = StepByStep(model, loss, optimizer)
+sbs_seq_selfattnpe.set_loaders(train_loader, test_loader)
+sbs_seq_selfattnpe.train(100)
+print(sbs_seq_selfattnpe.losses[-1], sbs_seq_selfattnpe.val_losses[-1])
